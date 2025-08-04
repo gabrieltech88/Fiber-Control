1d5c64c24b7789c7538f67a596cb06d1221e0ec2
Gabriel Ramos <gabrieltech209@gmail.com>
2025-08-04 01:05:53 -0300

    Ajuste no front-end e no .gitignote

--- FiberControl.Api/obj/Debug/net9.0/FiberControl.Api.AssemblyInfo.cs
+++ FiberControl.Api/obj/Debug/net9.0/FiberControl.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FiberControl.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d574f1cbf8ba65fed46b6a4a8cee33487375d992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aaf0a01ec6541f8c8c44869ec7ceef10327cb27")]
 [assembly: System.Reflection.AssemblyProductAttribute("FiberControl.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FiberControl.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
